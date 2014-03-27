9a031486ddcd637e694ac4230e1976bfcce0c7e1
Fernando Redondo <fredondo@pronoide.com>
2014-03-27 10:34:47 +0100

    complete domain class properties and other stuff

--- grails-app/domain/sesamestreet/Muppet.groovy
+++ grails-app/domain/sesamestreet/Muppet.groovy
@@ -2,6 +2,32 @@ package sesamestreet
 
 class Muppet {
 
-    static constraints = {
-    }
+	final static Character MALE = 'M'
+	final static Character FEMALE = 'F'
+	final static Character ANY = '%'
+
+	String name
+	String about
+	String color
+	Character gender
+	boolean hasFur = Boolean.FALSE
+	String picture
+	static hasMany = [clips: String]
+
+	static constraints = {
+		name nullable: true, size: 2..15
+		about nullable: true, size: 0..1000
+		color blank: false
+		gender nullable: true
+		picture nullable: true 
+		clips nullable: true
+	}
+
+	static mapping = {
+		table 'muppets', 
+			id_column: 'muppet_id',
+			about: [length: 1000]
+		clips joinTable: 
+			[name: 'muppets_videoclips', key: 'muppet_id', column: 'clip_url', type: "text"]
+	}
 }
