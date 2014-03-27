14a6e1bfbbae645dea0c02e1a80f4be4f5c5da82
Fernando Redondo <fredondo@pronoide.com>
2014-03-27 14:49:13 +0100

    controller implementation

--- grails-app/controllers/sesamestreet/MuppetDiscoveryController.groovy
+++ grails-app/controllers/sesamestreet/MuppetDiscoveryController.groovy
@@ -2,5 +2,35 @@ package sesamestreet
 
 class MuppetDiscoveryController {
 
-    def index() { }
+    def muppetDiscoveryService
+	def desiredTraits
+	
+	static scope = 'session'
+	
+    def index() {
+		render view: 'choosemuppettraits'
+	}
+	
+	def search(){
+		desiredTraits = new Muppet(params)
+		
+		if(!desiredTraits.validate()) {
+			return redirect (action: 'index')
+		}
+		
+		log.debug "searching perfect match for ${desiredTraits.properties}..."
+		flash.msg = "Congrats We've found the perfect muppet for you!"
+		def targetView='found'
+		def perfectMuppet = muppetDiscoveryService.lookForYourPerfectMuppet desiredTraits
+		def relatedMuppets
+		
+		if(!perfectMuppet){
+			log.debug "searching related muppets for  ${desiredTraits.properties}..."
+			flash.msg = "Sorry We haven't found the perfect muppet for you!"
+			targetView='notfound'
+			relatedMuppets = muppetDiscoveryService.lookForRelatedMuppets desiredTraits
+		}
+		
+		render view:targetView , model:[muppet:perfectMuppet,muppets:relatedMuppets]
+	}
 }
