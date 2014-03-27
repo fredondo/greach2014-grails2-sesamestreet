5fd4ee5bc0eb2c7d9b0963b337f850224fdcc555
Fernando Redondo <fredondo@pronoide.com>
2014-03-27 13:10:44 +0100

    create service skeleton

---
+++ grails-app/services/sesamestreet/MuppetDiscoveryService.groovy
@@ -0,0 +1,14 @@
+package sesamestreet
+
+import java.util.Set;
+
+class MuppetDiscoveryService {
+
+	Muppet lookForYourPerfectMuppet(Muppet desiredTraitsExample) {
+
+	}
+
+	Set<Muppet> lookForRelatedMuppets(Muppet desiredTraitsExample) {
+
+	}
+}
